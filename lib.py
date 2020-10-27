0e6a5b0b5a73da10a0123aadc90546d2bb823f8b
Quanlai Li <qli@lyft.com>
2020-10-27 14:04:25 -0400

    Add new data

--- lib.py
+++ lib.py
@@ -49,12 +49,15 @@ class Bill:
         total_person_days = 0
         tenant_days = {}
         actual_period = {}
+        # print('*'*8)
         for tenant in tenants:
             days = (min(tenant.end, self.end) - max(tenant.start, self.start)).days + 1
             actual_period[tenant] = "Actual Period: %s ~ %s" % \
                                     (str(max(tenant.start, self.start).date()), str(min(tenant.end, self.end).date()))
             if days < 0:
                 days = 0
+            # print(tenant.name, days)
+            # print(tenant.end, min(tenant.end, self.end), max(tenant.start, self.start))
             total_person_days += days
             tenant_days[tenant] = days
         # print(self.name)
@@ -80,32 +83,32 @@ class Bill:
                         "%s\n\t%s\n\t%s\n\t%.2f*%d" %
                         (self.name, self.billing_period, actual_period, amount_per_day, days))
 
-
-xu_chen = Tenant("Xu Chen", start=datetime(2019, 1, 1))
-zewei_chen = Tenant("Zewei Chen", start=datetime(2019, 1, 1))
-quanlai_li = Tenant("Quanlai Li", start=datetime(2019, 9, 1), end=datetime(2019, 12, 8))
-ping_hu = Tenant("Ping Hu", start=datetime(2019, 5, 10), end=datetime(2019, 8, 25))
-yuchen_guo = Tenant("Yuchen Guo", start=datetime(2019, 11, 1))
-
-Bill(start=datetime(2019, 10, 1), end=datetime(2019, 12, 10), name="Internet")\
-    .charge_per_day(0.5)
-
-Bill(start=datetime(2019, 10, 1), end=datetime(2019, 12, 10), name="Cleaning")\
-    .charge_per_day(0.33)
-
-Bill(start=datetime(2019, 9, 4), end=datetime(2019, 10, 1), name="PGE")\
-    .charge_per_usage(48.33)
-Bill(start=datetime(2019, 10, 2), end=datetime(2019, 10, 30), name="PGE")\
-    .charge_per_usage(73.01)
-
-Bill(start=datetime(2019, 8, 15), end=datetime(2019, 10, 15), name="Water")\
-    .charge_per_usage(184.51)
-
-# Bill(start=datetime(2019, 10, 1), end=datetime(2019, 12, 10), name="Garbage")\
-#     .charge_per_day(0.3)
-
-quanlai_li.check()
-xu_chen.check()
-zewei_chen.check()
-ping_hu.check()
-yuchen_guo.check()
+#
+# xu_chen = Tenant("Xu Chen", start=datetime(2019, 1, 1))
+# zewei_chen = Tenant("Zewei Chen", start=datetime(2019, 1, 1))
+# quanlai_li = Tenant("Quanlai Li", start=datetime(2019, 9, 1), end=datetime(2019, 12, 8))
+# ping_hu = Tenant("Ping Hu", start=datetime(2019, 5, 10), end=datetime(2019, 8, 25))
+# yuchen_guo = Tenant("Yuchen Guo", start=datetime(2019, 11, 1))
+#
+# Bill(start=datetime(2019, 10, 1), end=datetime(2019, 12, 10), name="Internet")\
+#     .charge_per_day(0.5)
+#
+# Bill(start=datetime(2019, 10, 1), end=datetime(2019, 12, 10), name="Cleaning")\
+#     .charge_per_day(0.33)
+#
+# Bill(start=datetime(2019, 9, 4), end=datetime(2019, 10, 1), name="PGE")\
+#     .charge_per_usage(48.33)
+# Bill(start=datetime(2019, 10, 2), end=datetime(2019, 10, 30), name="PGE")\
+#     .charge_per_usage(73.01)
+#
+# Bill(start=datetime(2019, 8, 15), end=datetime(2019, 10, 15), name="Water")\
+#     .charge_per_usage(184.51)
+#
+# # Bill(start=datetime(2019, 10, 1), end=datetime(2019, 12, 10), name="Garbage")\
+# #     .charge_per_day(0.3)
+#
+# quanlai_li.check()
+# xu_chen.check()
+# zewei_chen.check()
+# ping_hu.check()
+# yuchen_guo.check()
